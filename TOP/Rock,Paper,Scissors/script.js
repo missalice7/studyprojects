db050ddedd8cae42912fe3778740061e982184a2
missalice77 <sampaio.j.alice@gmail.com>
2020-06-04 19:45:54 +0100

    js over
    finished the javascript part :D the game is fully functional

--- TOP/Rock,Paper,Scissors/script.js
+++ TOP/Rock,Paper,Scissors/script.js
@@ -9,15 +9,18 @@ function computerPlay(){
 // VARIABLES
 let playerScore = 0;
 let computerScore = 0;
-let round = 0;
+let round = 1;
 
-let playerScoreText = document.getElementById("playerScore");
-let computerScoreText = document.getElementById("computerScore");
-let roundScoreText = document.getElementById("round-score");
+let roundText = document.getElementById("round_update");
+let playerScoreText = document.getElementById("player_score");
+let computerScoreText = document.getElementById("computer_score");
+let roundScoreText = document.getElementById("round_score");
+let totalScoreText = document.getElementById("total_score");
 
 let rockbtn = document.getElementById("rock");
 let paperbtn = document.getElementById("paper");
 let scissorsbtn = document.getElementById("scissors");
+let resetbtn = document.getElementById("reset");
 
 
 // LET'S PLAY
@@ -30,6 +33,7 @@ function playRound(playerSelection, computerSelection){
         playerScore +=1;
         round+=1;
         playerScoreText.innerHTML = playerScore;
+        roundText.innerHTML = round;
         roundScoreText.innerHTML = "You win this round!";
         finishRound()
         return;
@@ -38,6 +42,7 @@ function playRound(playerSelection, computerSelection){
         playerScore +=1;
         round+=1;
         playerScoreText.innerHTML = playerScore;
+        roundText.innerHTML = round;
         roundScoreText.innerHTML = "You win this round!";
         finishRound()
         return;
@@ -46,12 +51,14 @@ function playRound(playerSelection, computerSelection){
         playerScore +=1;
         round+=1;
         playerScoreText.innerHTML = playerScore;
+        roundText.innerHTML = round;
         roundScoreText.innerHTML = "You win this round!";
         finishRound()
         return;
 
     }if(playerSelection === computerSelection){
         round+=1;
+        roundText.innerHTML = round;
         roundScoreText.innerHTML = "It's a tie.";
         finishRound()
         return;
@@ -59,6 +66,7 @@ function playRound(playerSelection, computerSelection){
         round+=1
         computerScore +=1
         computerScoreText.innerHTML = computerScore;
+        roundText.innerHTML = round;
         roundScoreText.innerHTML = "Player 2 wins this round.";
         finishRound()
         return;
@@ -70,11 +78,27 @@ function playRound(playerSelection, computerSelection){
 // FINISH
 
 function finishRound(){
-    if(round === 5){
+    if(round === 6){
         rockbtn.disabled = true;
         paperbtn.disabled = true;
         scissorsbtn.disabled = true;
+        roundText.innerHTML = round-1;
+        resetbtn.style.display = "block";
+        if(playerScore>computerScore){
+            totalScoreText.innerHTML = "You WON"
+            return;
+        }if(computerScore>playerScore){
+            totalScoreText.innerHTML = "You LOST"
+            return;
+        }else{
+            totalScoreText.innerHTML = "TIE LOL"
+            return;
+        }
         return;
     }
     return;
+}
+
+function resetGame(){
+    location.reload()
 }
