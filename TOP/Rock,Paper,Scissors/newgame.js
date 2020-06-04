51cc40d9a6b615b4de558893ae5bd0b1af8b6d15
missalice77 <sampaio.j.alice@gmail.com>
2020-06-04 18:55:29 +0100

    working scores
    was able to add scores to the text in html

--- TOP/Rock,Paper,Scissors/newgame.js
+++ TOP/Rock,Paper,Scissors/newgame.js
@@ -1,42 +1,49 @@
 //What pc chooses
 function computerPlay(){
     let pcHand = ["rock", "paper","scissors"]
-    let getHand = pcHand[Math.floor(Math.random() * hand.length)];
+    let getHand = pcHand[Math.floor(Math.random() * pcHand.length)];
     return getHand;
 }
 
 
 
-// SPARATION LETS
+// SEPARATION LETS
 let playerScore = 0;
 let computerScore = 0;
-let round = 0
+let round = 0;
+
+let playerScoreText = document.getElementById("playerScore")
+let computerScoreText = document.getElementById("computerScore")
 
 let rockbtn = document.getElementById("rock");
 let paperbtn = document.getElementById("paper");
 let scissorsbtn = document.getElementById("scissors");
 
-rockbtn.onclick = playRound("rock", computerSelection)
-paperbtn.onclick = playRound("paper", computerSelection)
-scissorsbtn.onclick =  playRound("rock", computerSelection)
 
 // LET'S PLAY
 
 function playRound(playerSelection, computerSelection){
-    let computerSelection = computerPlay()
+    computerSelection = computerPlay()
 
-    (playerSelection == "rock" && computerSelection == "scissors") ? "win"
-    
+    if(playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore +=1
+        round+=1
+        playerScoreText.innerHTML = playerScore
 
-    :(playerSelection == "paper" && computerSelection == "rock") ? "win"
-
-
-    :(playerSelection == "rock" && computerSelection == "scissors") ? "win"
-
-
-    :(playerSelection == computerSelection) ?  "tie"
-
-
-    :"lose"
+    }if(playerSelection == "paper" && computerSelection == "rock"){
+        playerScore +=1
+        round+=1
+        playerScoreText.innerHTML = playerScore
+    }if(playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore +=1
+        round+=1
+        playerScoreText.innerHTML = playerScore
+    }if(playerSelection == computerSelection){
+        round+=1
+    }else{
+        round+=1
+        computerScore +=1
+        computerScoreText.innerHTML = computerScore
+    }
     
 }
