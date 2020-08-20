6d13b60e3142285ff68e02357a6461cfd8a8fb24
missalice77 <sampaio.j.alice@gmail.com>
2020-08-20 19:05:13 +0100

    library
    Started library with bootstrap for top

---
+++ TOP/Library/index.js
@@ -0,0 +1,28 @@
+let myLibrary = [
+    {
+        title: "Harry Potter and the Philosopher's Stone",
+        author: "J.K. Rowling",
+        cover: "https://images-na.ssl-images-amazon.com/images/I/51mtZy7oJVL.jpg",
+        read: true,
+    },
+    {
+        title: "Twenty Thousand Leagues Under the Sea",
+        author: "Jules Verne",
+        cover: "https://upload.wikimedia.org/wikipedia/commons/1/10/Houghton_FC8_V5946_869ve_-_Verne%2C_frontispiece.jpg",
+        read: false,
+    }
+];
+
+
+
+function Book(title,author,read){
+    this.title = title
+    this.author = author
+    this.cover = cover
+    this.read = read
+    
+}
+
+function addBookToLibrary() {
+    console.log("working")
+  }
