d72ad4a66e97a363aac46cc604e7d9f5c44935fb
missalice77 <sampaio.j.alice@gmail.com>
2020-08-20 21:16:38 +0100

    adjust css
    adjusting bootstrap

--- TOP/Library/index.js
+++ TOP/Library/index.js
@@ -1,28 +1,38 @@
-let myLibrary = [
-    {
-        title: "Harry Potter and the Philosopher's Stone",
-        author: "J.K. Rowling",
-        cover: "https://images-na.ssl-images-amazon.com/images/I/51mtZy7oJVL.jpg",
-        read: true,
-    },
-    {
-        title: "Twenty Thousand Leagues Under the Sea",
-        author: "Jules Verne",
-        cover: "https://upload.wikimedia.org/wikipedia/commons/1/10/Houghton_FC8_V5946_869ve_-_Verne%2C_frontispiece.jpg",
-        read: false,
-    }
-];
+const BOOK_BTN = document.getElementById('bookBtn')
 
 
 
-function Book(title,author,read){
+function Book(title,author,cover){
     this.title = title
     this.author = author
     this.cover = cover
-    this.read = read
     
 }
 
+function createCard (){
+    const div = document.createElement('div');
+
+}
+
+
 function addBookToLibrary() {
-    console.log("working")
+    let newTitle = document.querySelector('.new-title')
+    let newAuthor = document.querySelector('.new-author')
+    let newCover = document.querySelector('.new-cover')
   }
+
+
+let library = [
+        {
+            title: "Harry Potter and the Philosopher's Stone",
+            author: "J.K. Rowling",
+            cover: "https://images-na.ssl-images-amazon.com/images/I/51mtZy7oJVL.jpg",
+            read: true,
+        },
+        {
+            title: "Twenty Thousand Leagues Under the Sea",
+            author: "Jules Verne",
+            cover: "https://upload.wikimedia.org/wikipedia/commons/1/10/Houghton_FC8_V5946_869ve_-_Verne%2C_frontispiece.jpg",
+            read: false,
+        }
+    ];
