d46986cadc958bee2ca89d037fb3c4059877692d
missalice77 <sampaio.j.alice@gmail.com>
2020-08-21 16:59:18 +0100

    testing phase after this
    finished functions. testing time

--- TOP/Library/index.js
+++ TOP/Library/index.js
@@ -1,38 +1,72 @@
-const BOOK_BTN = document.getElementById('bookBtn')
+const BOOK_BTN = document.getElementById('BOOK_BTN');
+const LIBRARY_DIV = document.querySelector('.library');
+let lastCardDeck = LIBRARY_DIV.lastElementChild;
 
-
+let library = [];
 
 function Book(title,author,cover){
-    this.title = title
-    this.author = author
+    this.title = title,
+    this.author = author,
     this.cover = cover
-    
 }
 
-function createCard (){
-    const div = document.createElement('div');
+function addNewBook() {
+    let newTitle = document.querySelector('.new-title').value
+    let newAuthor = document.querySelector('.new-author').value
+    let newCover = document.querySelector('.new-cover').value
+
+    let newBook = new Book (newTitle, newAuthor, newCover)
+    library.push(newBook)
+
+    return newBook;    
+}
+
+function createCard (newbook){
+    let div = document.createElement('div');
+    let div2 = document.createElement('div');
+    let img = document.createElement('img');
+    let p = document.createElement('p');
+    let p2 = document.createElement('p');
+    let button = document.createElement('button');
+    let button2 = document.createElement('button');
+
+    let cardDiv = lastCardDeck.appendChild(div)
+    cardDiv.classList.add('card')
+
+    let imgCover = cardDiv.appendChild(img)
+    imgCover.src = newbook.cover
+    imgCover.classList.add('card-img-top')
+    imgCover.alt = "cover"
+
+    let cardBody = cardDiv.appendChild(div2)
+    cardBody.classList.add('card-body')
+
+    let cardTitle = cardBody.appendChild(p)
+    let cardAuthor = ardBody.appendChild(p2)
+    let readBtn = cardBody.appendChild(button)
+    let deleteBtn = cardBody.appendChild(button2)
+
+    cardTitle.classList.add('card-title', 'font-weight-bold')
+    cardTitle.innerText = newbook.title
+
+    cardAuthor.classList.add('card-text')
+    cardAuthor.innerText = newbook.author
+
+    readBtn.classList.add('btn', 'btn-danger', 'btn-sm')
+    readBtn.type = 'button'
+    readBtn.innerText = 'Not Read'
+
+    deleteBtn.classList.add('btn', 'btn-danger', 'btn-sm', 'float-right')
+    deleteBtn.type = 'button'
+    deleteBtn.innerText = 'Delete'
+
+    return cardDiv;
 
 }
 
+function addBookToLibrary(){
+    addNewBook()
+    createCard(newBook)
+}
 
-function addBookToLibrary() {
-    let newTitle = document.querySelector('.new-title')
-    let newAuthor = document.querySelector('.new-author')
-    let newCover = document.querySelector('.new-cover')
-  }
-
-
-let library = [
-        {
-            title: "Harry Potter and the Philosopher's Stone",
-            author: "J.K. Rowling",
-            cover: "https://images-na.ssl-images-amazon.com/images/I/51mtZy7oJVL.jpg",
-            read: true,
-        },
-        {
-            title: "Twenty Thousand Leagues Under the Sea",
-            author: "Jules Verne",
-            cover: "https://upload.wikimedia.org/wikipedia/commons/1/10/Houghton_FC8_V5946_869ve_-_Verne%2C_frontispiece.jpg",
-            read: false,
-        }
-    ];
+BOOK_BTN.addEventListener("click", addBookToLibrary)
