9ec12d830fc4f727fbb75993e8a4c9abafde5517
missalice77 <sampaio.j.alice@gmail.com>
2020-08-21 22:32:35 +0100

    no js
    changing the btn to run without bootstrap javascript

--- TOP/Library/index.js
+++ TOP/Library/index.js
@@ -1,13 +1,15 @@
 const BOOK_BTN = document.getElementById('BOOK_BTN');
-const LIBRARY_DIV = document.querySelector('.library');
-let lastCardDeck = LIBRARY_DIV.lastElementChild;
+let libraryDiv = document.querySelector('.library');
+let lastCardDeck = libraryDiv.lastElementChild;
+
 
 let library = [];
 
-function Book(title,author,cover){
+function Book(title,author,cover,read){
     this.title = title,
     this.author = author,
     this.cover = cover
+    this.read = read
 }
 
 function addNewBook() {
@@ -18,10 +20,10 @@ function addNewBook() {
     let newBook = new Book (newTitle, newAuthor, newCover)
     library.push(newBook)
 
-    return newBook;    
+    return newBook; 
 }
 
-function createCard (newbook){
+function createCard (book){
     let div = document.createElement('div');
     let div2 = document.createElement('div');
     let img = document.createElement('img');
@@ -30,42 +32,86 @@ function createCard (newbook){
     let button = document.createElement('button');
     let button2 = document.createElement('button');
 
+    
+
+    if (lastCardDeck.childElementCount == 4){
+         lastCardDeck = libraryDiv.appendChild(document.createElement('div'))
+         lastCardDeck.classList.add('card-deck', 'mx-auto')
+        }
+
+
     let cardDiv = lastCardDeck.appendChild(div)
     cardDiv.classList.add('card')
 
     let imgCover = cardDiv.appendChild(img)
-    imgCover.src = newbook.cover
+    imgCover.src = book.cover
     imgCover.classList.add('card-img-top')
-    imgCover.alt = "cover"
+    imgCover.alt = book.title
 
     let cardBody = cardDiv.appendChild(div2)
     cardBody.classList.add('card-body')
 
     let cardTitle = cardBody.appendChild(p)
-    let cardAuthor = ardBody.appendChild(p2)
+    let cardAuthor = cardBody.appendChild(p2)
     let readBtn = cardBody.appendChild(button)
     let deleteBtn = cardBody.appendChild(button2)
 
     cardTitle.classList.add('card-title', 'font-weight-bold')
-    cardTitle.innerText = newbook.title
+    cardTitle.innerText = book.title
 
     cardAuthor.classList.add('card-text')
-    cardAuthor.innerText = newbook.author
-
-    readBtn.classList.add('btn', 'btn-danger', 'btn-sm')
+    cardAuthor.innerText = book.author
+        
+    readBtn.classList.add('read-btn', 'btn', 'btn-sm')
     readBtn.type = 'button'
-    readBtn.innerText = 'Not Read'
+    
+    let readCheckbox = document.getElementById('READ-CHECK')
+    if (readCheckbox.checked !== true){
+        readBtn.innerText = 'Not Read'
+        readBtn.classList.add('btn-danger')
+    }
+    if(readCheckbox.checked === true){
+        readBtn.innerText = 'Read'
+        readBtn.classList.add('btn-success')
+    }
 
-    deleteBtn.classList.add('btn', 'btn-danger', 'btn-sm', 'float-right')
+    changeReadBtn(readBtn,book)
+
+    deleteBtn.classList.add('btn', 'btn-danger', 'btn-sm', 'float-right','delete-btn')
     deleteBtn.type = 'button'
     deleteBtn.innerText = 'Delete'
 
     return cardDiv;
 
+
+}
+
+function changeReadBtn(button){
+
+    button.addEventListener('click', () =>{
+        if (button.innerText == 'Not Read') {
+            button.innerText = "Read"
+            button.classList.remove('btn-danger')
+            button.classList.add('btn-success')
+            return;
+
+        }else{
+            button.innerText = "Not Read"
+            button.classList.remove('btn-success')
+            button.classList.add('btn-danger')
+            return;
+        }
+    })
+}
+
+function deleteBook(){
+
+    let deleteBtn = document.querySelectorAll('.delete-btn')
+
 }
 
 function addBookToLibrary(){
-    addNewBook()
+    let newBook = addNewBook()
     createCard(newBook)
 }
 
